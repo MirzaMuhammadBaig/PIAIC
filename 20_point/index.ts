805ac2e669b1e9cdbc23501a7011e7d01a2d4922
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 19:25:38 +0500

    add_question_in_script

--- 20_point/index.ts
+++ 20_point/index.ts
@@ -1,3 +1,5 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
 let mountains: string[] = ['Mount Everest', 'K2', 'Kangchenjunga', 'Matterhorn', 'Mount Kilimanjaro'];
 let rivers: string[] = ['Nile', 'Amazon', 'Yangtze', 'Mississippi', 'Danube'];
 let countries: string[] = ['Afghanistan', 'Pakistan', 'Turkey', 'Iraq', 'Palestine'];
