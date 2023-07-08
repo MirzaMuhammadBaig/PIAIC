c0513b6943b3466680bf51d93868a77dca0e369d
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 15:59:00 +0500

    point_25_done

---
+++ 25_Alien_Colors_#1/index.ts
@@ -0,0 +1,9 @@
+let alien_color: string = 'green';
+
+if (alien_color == 'green') {
+  console.log("Congratulations! You just earned 5 points!");
+}
+
+if (alien_color === 'yellow') {
+    console.log("The player just earned 5 points.");
+}
