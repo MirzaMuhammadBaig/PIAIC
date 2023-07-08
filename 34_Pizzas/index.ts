ac18e264a9800dd0994f7b5342ff8e9320fc43f1
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 17:22:12 +0500

    point_34_done

---
+++ 34_Pizzas/index.ts
@@ -0,0 +1,9 @@
+let pizzas: string[] = ["Pepperoni", "Margherita", "Supreme"];
+
+for (let i = 0; i < pizzas.length; i++) {
+  console.log(`I like ${pizzas[i]} pizza.`);
+}
+
+console.log(
+  "Pizza is such a versatile and delicious dish that never fails to satisfy my cravings. The combination of gooey cheese, flavorful sauce, and a crispy crust is simply irresistible. Pizza brings people together and makes every gathering more enjoyable. I really love pizza!"
+);
