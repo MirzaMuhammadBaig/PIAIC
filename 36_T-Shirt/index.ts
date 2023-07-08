91db957fe90810ffaacf892f1c16483e7963c76d
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 18:24:32 +0500

    point_36_done

---
+++ 36_T-Shirt/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string | number, message: string) {
+    console.log(`A ${size} T-shirt will be printed with the message: "${message}".`);
+  }
+  
+  make_shirt("Large", "I love coding!");
+  
