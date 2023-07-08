521f26528e08efc74346dc4635b306f05da01550
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 19:16:02 +0500

    add_question_in_script

--- 13_Your_Own_Array/index.ts
+++ 13_Your_Own_Array/index.ts
@@ -1,3 +1,5 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
 let favoriteTransportation: string[] = ['motorcycle', 'car', 'bicycle'];
 
 for (let i = 0; i < favoriteTransportation.length; i++) {
