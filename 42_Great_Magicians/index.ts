0ead5210e0e956cff08f5849d4c2d92d18ede02c
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 20:13:38 +0500

    add_question_in_script

--- 42_Great_Magicians/index.ts
+++ 42_Great_Magicians/index.ts
@@ -1,3 +1,5 @@
+// Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
 function showMagicians(magicians: string[]): void {
     for (const magician of magicians) {
       console.log(magician);
