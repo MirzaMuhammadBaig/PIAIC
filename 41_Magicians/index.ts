fc93c1dd3332d2668dd97e7d3521209eb14f5bfb
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 18:49:28 +0500

    point_41_done

---
+++ 41_Magicians/index.ts
@@ -0,0 +1,9 @@
+function show_magicians(magicians: string[]): void {
+    for (const magician of magicians) {
+      console.log(magician);
+    }
+  }
+  
+  const magicianNames: string[] = ["Magician 1", "Magician 2", "Magician 3", "Magician 4"];
+  show_magicians(magicianNames);
+  
