b4547c94df32753334d84a6d8db3e3c6b2524706
Muhammad <muhammadbaig090@gmail.com>
2023-07-08 09:11:21 +0500

    point_14_done

---
+++ 14_Guest_List/index.ts
@@ -0,0 +1,5 @@
+let guestList: string[] = ['Mirza', 'Muhammad', 'Baig'];
+
+for (let i = 0; i < guestList.length; i++) {
+  console.log(`\nDear ${guestList[i]},\n You are invited to dinner. \nSincerely,\n Blockchain Dev \n______________________________`);
+}
