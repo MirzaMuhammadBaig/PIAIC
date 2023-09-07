760ce43bfe0a99dffba8eaf8b319324b376b25a4
MirzaMuhammadBaig <mirzamuhammadbaig328@gmail.com>
2023-09-07 22:54:13 +0500

    build-cli-calculator-&-first-assignment

--- First-Assignemnt/14_Guest_List/index.js
+++
@@ -1,4 +0,0 @@
-var guestList = ['Mirza', 'Muhammad', 'Baig'];
-for (var i = 0; i < guestList.length; i++) {
-    console.log("\nDear ".concat(guestList[i], ",\n You are invited to dinner. \nSincerely,\n Blockchain Dev \n______________________________"));
-}
--- First-Assignemnt/9_Favorite_Number/index.js
+++
@@ -1,4 +0,0 @@
-// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-var favNum = 678;
-var msg = "My favorite number is ".concat(favNum, ".");
-console.log(msg);
